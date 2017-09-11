d1db06458563a8ca4106436055e72191d37d5780
Laryssa Seabra <llaryssa@me.com>
2017-09-10 21:32:16 -0300

    add stereo matching opencv methods for computing disparoties

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ print np.array([[1,2],[3,4]])
 
 
 import preprocessing
-images, gt = preprocessing.readFiles("middleburry-third", ["Baby1", "Baby3"])
+images, gt = preprocessing.readFiles("middleburry-third", 3)
 print len(images), len(gt)
+
+disp = preprocessing.computeDisparities(images)
+print len(disp)
+
+for d in disp:
+    plt.imshow(d, 'gray')
+    plt.show()
--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,34 @@
 import cv2 as cv
+from glob import glob
 
 LEFT_IMAGE_FILENAME = "view1.png"
 RIGHT_IMAGE_FILENAME = "view5.png"
 DISPARITY_FILENAME = "disp1.png"
 
-def readFiles(folderPath, subfolders):
+WINDOW_SIZE = 7
+# MAX_DISPARITY = 155
+MAX_DISPARITY = 64
+STEREO_BLOCK_SIZE = 9
+
+def readFiles(folderPath, quantity):
     images = list()
     groundtruth = list()
+    subfolders = glob(folderPath + "/*/")[:quantity]
     for folder in subfolders:
-        lft = cv.imread(folderPath+'/'+folder+'/'+LEFT_IMAGE_FILENAME, cv.IMREAD_GRAYSCALE)
-        rgt = cv.imread(folderPath+'/'+folder+'/'+RIGHT_IMAGE_FILENAME, cv.IMREAD_GRAYSCALE)
-        dsp = cv.imread(folderPath+'/'+folder+'/'+DISPARITY_FILENAME, cv.IMREAD_GRAYSCALE)
+        lft = cv.imread(folder + LEFT_IMAGE_FILENAME, cv.IMREAD_GRAYSCALE)
+        rgt = cv.imread(folder + RIGHT_IMAGE_FILENAME, cv.IMREAD_GRAYSCALE)
+        dsp = cv.imread(folder + DISPARITY_FILENAME, cv.IMREAD_GRAYSCALE)
         images.append((lft, rgt))
         groundtruth.append(dsp)
     return images, groundtruth
+
+def computeDisparities(images):
+    disparities = list()
+    stereoMatcher = cv.StereoBM_create(numDisparities=MAX_DISPARITY, blockSize=STEREO_BLOCK_SIZE)
+    # stereoMatcher = cv.StereoSGBM_create(numDisparities=MAX_DISPARITY, blockSize=STEREO_BLOCK_SIZE)
+    for (left, right) in images:
+        disparity = stereoMatcher.compute(left, right)
+        disparities.append(disparity)
+    return disparities
+
+def generateData(disparities, groundtruth):
